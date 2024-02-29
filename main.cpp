8ce9233379d9e8e32ca71770385ad2d98a027432
omkar3060 <153345475+omkar3060@users.noreply.github.com>
2024-02-29 22:56:33 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRB=0X0f;
+	PORTC=0X03;
+	while(1)
+	{
+		if(((PINC&0x01)==0x01)&&((PINC&0x02)==0x02))
+		{
+			PORTB=0x05;
+		}
+		if(((PINC&0x01)==0x01)&&((PINC&0x02)==0x00))
+		{
+			PORTB=0x01;
+		}
+		if(((PINC&0x01)==0x00)&&((PINC&0x02)==0x02))
+		{
+			PORTB=0x04;
+		}
+		if(((PINC&0x01)==0x00)&&((PINC&0x02)==0x00))
+		{
+			PORTB=0x00;
+		}
+	}
+}
